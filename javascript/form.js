b174a1be4c2a591db4ccb960de56e3e949690325
CrazyAl369 <dlfbonds@gmail.com>
2022-04-24 21:05:48 -0700

    Contact API Addition and Mobile View Update.

--- javascript/form.js
+++ javascript/form.js
@@ -1,11 +1,37 @@
-const form = document.querySelector('form')
-        form.addEventListener('submit', event => {
+const form = document.querySelector("form");
+        form.addEventListener("submit", (event) => {
         // prevent the form submit from refreshing the page
-        event.preventDefault()
-        
-        const { name, email, message } = event.target
-        console.log('Name: ', name.value)
-        console.log('email: ', email.value)
-        console.log('Message: ', message.value)
-        
-        })
+        event.preventDefault();
+
+        const { name, email, phone, subject, message } = event.target;
+
+            // Use your API endpoint URL you copied from the previous step
+        const endpoint =
+            "https://cgteifdep7.execute-api.us-west-1.amazonaws.com/ElCompaGrill-Stag/ElCompaGrill-Form-Lambda";
+        // We use JSON.stringify here so the data can be sent as a string via HTTP
+            const body = JSON.stringify({
+            senderName: name.value,
+            senderEmail: email.value,
+            senderPhone: phone.value,
+            senderSubject: subject.value,
+            message: message.value
+        });
+        const requestOptions = {
+            method: "POST",
+            body
+        };
+
+        fetch(endpoint, requestOptions)
+            .then((response) => {
+            if (!response.ok) throw new Error("Error in fetch");
+            return response.json();
+            })
+            .then((response) => {
+            document.getElementById("result-text").innerText =
+                "Email sent successfully!";
+            })
+            .catch((error) => {
+            document.getElementById("result-text").innerText =
+                "An unkown error occured.";
+            });
+        });
